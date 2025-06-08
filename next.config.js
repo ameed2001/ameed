98ec99367c835de1b39776ec1e0df73e3e62d379
Media Plus <mediaplus64@gmail.com>
2025-06-08 13:18:54 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-import type {NextConfig} from 'next';
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -27,7 +27,7 @@ const nextConfig: NextConfig = {
         hostname: 'upload.wikimedia.org',
         port: '',
         pathname: '/**',
-      }
+      },
     ],
   },
 };

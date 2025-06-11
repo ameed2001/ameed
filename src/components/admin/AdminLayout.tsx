0fa42873f9ec0d5df6933ce7a5ad475c534545e9
Media Plus <mediaplus64@gmail.com>
2025-06-11 10:04:09 +0000

    الشريط الجانبي في حساب الادمن اريده ان يلتصق بالاخر (اقصى اليمين العربي)

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -9,9 +9,9 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
     <div className="flex flex-col min-h-screen bg-transparent">
       <Header />
       {/* Not including main Navbar from AppLayout here, admin has its own sidebar */}
-      <div className="flex flex-1 container mx-auto mt-0"> {/* Removed py from AppLayout */}
-        <AdminSidebar />
-        <main className="flex-grow p-6 bg-background/80 shadow-inner rounded-r-lg"> {/* Added rounded-r-lg for style */}
+      <div className="flex flex-1 mt-0"> {/* Removed container mx-auto */}
+        <AdminSidebar /> {/* This will be on the right due to dir="rtl" */}
+        <main className="flex-grow p-6 bg-background/80 shadow-inner rounded-l-lg"> {/* Changed to rounded-l-lg */}
           {children}
         </main>
       </div>

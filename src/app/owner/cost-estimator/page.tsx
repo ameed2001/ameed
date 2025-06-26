f48dccf25df043a179027efa258a8f9b19836eba
Media Plus <mediaplus64@gmail.com>
2025-06-26 17:43:51 +0000

    لماذا ظهر شريط التنقل الذي في اقصى اليسار

--- src/app/owner/cost-estimator/page.tsx
+++ src/app/owner/cost-estimator/page.tsx
@@ -6,7 +6,7 @@ import SimpleCostEstimatorForm from '@/components/SimpleCostEstimatorForm';
 // This page is wrapped by the Owner layout by default, so it has the sidebar.
 export default function OwnerCostEstimatorPage() {
   return (
-    <div className="bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-800 dark:to-gray-900 -m-4 sm:-m-6 lg:-m-8 p-4 sm:p-6 lg:p-8">
+    <div className="bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-800 dark:to-gray-900 p-4 sm:p-6 lg:p-8">
       <SimpleCostEstimatorForm />
     </div>
   );

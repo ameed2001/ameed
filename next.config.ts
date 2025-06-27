cbb54a3fead6df0f3a164a56354e0277f09770c2
Media Plus <mediaplus64@gmail.com>
2025-06-27 11:35:12 +0000

    النظام الحالي يسبب هذه المشكلة Download the React DevTools for a better

--- next.config.ts
+++ next.config.ts
@@ -38,7 +38,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' data:;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.cloudworkstations.dev; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com https://*.cloudworkstations.dev; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' data: https://i.imgur.com https://placehold.co https://upload.wikimedia.org; connect-src 'self' wss://*.cloudworkstations.dev https://*.cloudworkstations.dev;",
           },
           {
             key: 'Set-Cookie',
